dc437b857f6581a5b517e94f280e8b4ba97f8eb3
Justin Yue <justinyue1643@gmail.com>
2024-07-26 21:15:20 -0700

    remove unnecessary prints

--- src/jax_rl/train_cartpole.py
+++ src/jax_rl/train_cartpole.py
@@ -9,18 +9,10 @@ def train_cartpole():
 
     # Instantiate the environment & its settings.
     env, env_params = gymnax.make("CartPole-v1")
-    print(env_params)
-    print("Observation space:", env.observation_space(env_params).low, env.observation_space(env_params).high)
-    print("Observation space:", env.observation_space(env_params).shape)
-    print("Action space:", env.action_space())
-    print("Action space:", env.action_space().n)
-    print("Action space:", env.action_space().shape)
 
     # Reset the environment.
     rng, key_reset = jax.random.split(rng, 2)
     obs, env_state = env.reset(key_reset, env_params)
-    print("Observation:", obs)
-    print("State:", env_state)
 
     jit_step = jax.jit(env.step)
     all_states = [(env_state.x, env_state.theta)]
--- src/jax_rl/train_pendulum.py
+++ src/jax_rl/train_pendulum.py
@@ -11,24 +11,8 @@ def visualize_example():
 
     # Instantiate the environment & its settings.
     env, env_params = gymnax.make("Pendulum-v1")
-    print(env_params)
-    print("Observation space:", env.observation_space(env_params).low, env.observation_space(env_params).high)
-    print("Observation space:", env.observation_space(env_params).shape)
-    print("Action space:", env.action_space().low, env.action_space().high)
-    print("Action space:", env.action_space().shape)
-
-    # Reset the environment.
-    obs, state = env.reset(key_reset, env_params)
-    print("Observation:", obs)
-    print("State:", state)
-
-    # Sample a random action.
-    action = env.action_space(env_params).sample(key_act)
-    jax.debug.print("{}", action)
 
     # Perform the step transition.
-    n_obs, n_state, reward, done, _ = env.step(key_step, state, action, env_params)
-    state_seq, reward_seq = [], []
     rng, rng_reset = jax.random.split(rng)
     obs, env_state = env.reset(rng_reset, env_params)
     all_thetas = [env_state.theta]
@@ -36,13 +20,11 @@ def visualize_example():
     jit_step = jax.jit(env.step)
 
     while True:
-        state_seq.append(env_state)
         rng, rng_act, rng_step = jax.random.split(rng, 3)
         action = env.action_space(env_params).sample(rng_act)
         next_obs, next_env_state, reward, done, info = jit_step(
             rng_step, env_state, action, env_params
         )
-        reward_seq.append(reward)
         all_thetas.append(next_env_state.theta)
         if done:
             break
@@ -50,7 +32,6 @@ def visualize_example():
             obs = next_obs
         env_state = next_env_state
 
-    cum_rewards = jnp.cumsum(jnp.array(reward_seq))
     visualizer = PendulumVisualizer(all_thetas)
     visualizer.visualize()
 
